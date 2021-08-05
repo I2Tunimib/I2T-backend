761aa6e1e68b58184b3bb94a310b46289888bf3e
Marco <m.ripamonti@campus.unimib.it>
2021-08-05 21:54:42 +0200

    Refactor with new table structure

--- routes/saved/all-saved.js
+++ routes/saved/all-saved.js
@@ -1,15 +1,9 @@
 module.exports = (router, fs) => {
     router.get('/saved', (req, res) => {
-        fs.readdir('./public/saved', (err, files)=>{
-            if(err) {
-                res.send({error: err});
-                return;
-            }
-            const tableArr = [];
-            files.forEach(file => {
-                tableArr.push(file.split('.')[0]);
-            });
-            res.send(tableArr);
-        })
+        const tables = fs.readdirSync('./public/saved', { withFileTypes: true })
+            .filter(dirent => dirent.isDirectory())
+            .map(dirent => dirent.name)
+        
+        res.json(tables);
     })
 }
--- routes/saved/put-saved.js
+++ routes/saved/put-saved.js
@@ -1,13 +1,35 @@
 module.exports = (router, fs, cors) => {
-    router.put('/saved/:name' ,cors(), (req, res) => {
-       const payload = req.body;
-       console.log( payload);
-       fs.writeFile(`./public/saved/${req.params.name}.json`, JSON.stringify(payload), (err) => {
-           if(err) {
-               res.send({error: err});
-           } else {
-               res.send({success: true})
-           }
-       })
+    const writeData = (path, filename, data) => {
+        return new Promise((resolve, reject) => {
+            fs.writeFile(`${path}/${filename}`, JSON.stringify(data), (err) => {
+                if (err)
+                    reject(err);
+                else
+                    resolve();
+            });
+        });
+    }
+
+    router.put('/saved/:name', cors(), (req, res) => {
+        const payload = req.body;
+
+        const pathToDir = `./public/saved/${req.params.name}`;
+
+
+        if (!fs.existsSync(pathToDir)){
+            console.log('HELLOOOO')
+            fs.mkdirSync(pathToDir);
+        }
+
+        const promises = Object.keys(payload).map(file => 
+            writeData(pathToDir, `${req.params.name}-${file}.json`, payload[file])
+        );
+
+        Promise.all(promises).then(() => {
+            res.json({success: true})         
+        }).catch(err => {
+            res.json({error: err}); 
+        });
+
     })
 }
