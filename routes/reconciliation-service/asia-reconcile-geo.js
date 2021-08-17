47eb3af43b7a4999f4de40f0a120f39d1f7429ff
Marco <m.ripamonti@campus.unimib.it>
2021-08-17 12:52:50 +0200

    Refacotor request Asia reconciliation

--- routes/reconciliation-service/asia-reconcile-geo.js
+++ routes/reconciliation-service/asia-reconcile-geo.js
@@ -3,43 +3,46 @@ const axios = require('axios').default;
 
 
 module.exports = (router) => {
-    router.post('/asia/reconcile/geonames', (req, res)=>{
-        const { data } = req.body;
+  router.post('/asia/reconcile/geonames', (req, res) => {
+    const { data } = req.body;
 
-        const request = data.map(col => {
-          const key = Object.keys(col)[0];
+    const queries = data.reduce((acc, { id, label }) => ({
+        ...acc,
+        [id]: { query: encodeURIComponent(label || '') }
+    }), {})
 
-          return col[key].reduce((acc, item) => {
-            acc[`${key}-${item.id}`] = {query: encodeURIComponent(item.label || '')}
-            return acc;
-          }, {})
+    // console.log(queries);
+
+    // const request = data.map(col => {
+    //   const key = Object.keys(col)[0];
+
+    //   return col[key].reduce((acc, item) => {
+    //     acc[`${key}-${item.id}`] = {query: encodeURIComponent(item.label || '')}
+    //     return acc;
+    //   }, {})
+    // })
+
+    // const queries = request.reduce((acc, colValue) => ({...acc, ...colValue}), {})
+
+    // const encodedKey = 'queries';
+    // const encodedValue = JSON.stringify(queries);
+    const formBody = 'queries=' + JSON.stringify(queries);
+    console.log(formBody);
+    axios.post(`${CONFIG.ASIA_RECONCILIATION}/geonames`, formBody)
+        .then((apiResponse)=> {
+          const { data } = apiResponse;
+
+          const response = Object.keys(data).reduce((acc, id)=> ({
+            ...acc,
+            [id]: {
+              metadata: data[id].result
+            }
+          }), {});
+          res.json(response);
         })
-
-        const queries = request.reduce((acc, colValue) => ({...acc, ...colValue}), {})
-        
-        const encodedKey = 'queries';
-        const encodedValue = JSON.stringify(queries);
-        const formBody = encodedKey + '=' + encodedValue;
-        axios.post(`${CONFIG.ASIA_RECONCILIATION}/geonames`, formBody)
-            .then((resp)=>{
-                const serviceData = resp.data;
-                
-                const response = data.reduce((acc, col) => {
-                  const key = Object.keys(col)[0];
-                  acc[key] = col[key].map((item, index) => (
-                    {
-                      id: item.id,
-                      metadata: serviceData[`${key}-${item.id}`].result || []
-                    }
-                  ))
-                  return acc;
-                }, {})
-
-                res.json(response);
-            })
-            .catch((err)=>{
-                // res.send({error: err});
-                console.log(err);
-            })
-    })
+        .catch((err)=>{
+            // res.send({error: err});
+            console.log(err);
+        })
+  })
 }
--- routes/tables/get-table.js
+++ routes/tables/get-table.js
@@ -1,15 +1,15 @@
 module.exports = (router, fs) => {
-    router.get('/tables/:id', (req, res) => {
-        fs.readFile(`./public/tables/${req.params.id}.csv`,"utf-8", (err, file) => {
-            if (err) {
-                res.send({error: err});
-                return;
-            }
-            const cleanFile = file.replace(/"+/g,'')
-            res.json({data: {
-              format: 'csv',
-              content: cleanFile
-            }});
-        }) 
+  router.get('/tables/:id', (req, res) => {
+    fs.readFile(`./public/tables/${req.params.id}.csv`, "utf-8", (err, file) => {
+      if (err) {
+        res.send({ error: err });
+        return;
+      }
+      const cleanFile = file.replace(/"+/g, '')
+      res.json({
+        format: 'csv',
+        data: cleanFile
+      });
     })
+  })
 }
