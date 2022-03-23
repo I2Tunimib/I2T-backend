169ea1559fbee013f08490e97ac640c105e276cf
Marco Ripamonti <m.ripamonti@campus.unimib.it>
2022-03-23 10:57:40 +0100

    Add early version of extension wikidataGeo

---
+++ src/services/extenders/wikidataGeoProperties/index.js
@@ -0,0 +1,37 @@
+export default {
+  private: {
+    endpoint: process.env.WD_ENTITY_DATA,
+    processRequest: true
+  },
+  public: {
+    name: 'Wikidata Geo Properties',
+    relativeUrl: '/wikidata/entities',
+    description: 'Geo property extension service: add properties associated with wikidata entities',
+    formParams: [
+      {
+        id: 'property',
+        description: 'Select on or more <b>Property</b> values:',
+        label: 'Property',
+        inputType: 'checkbox',
+        rules: ['required'],
+        options: [
+          {
+            id: 'P625',
+            label: 'Coordinate location (Lat & Lon)',
+            value: 'P625'
+          },
+          {
+            id: 'P421',
+            label: 'Time zone',
+            value: 'P421'
+          },
+          {
+            id: 'P281',
+            label: 'Postal code',
+            value: 'P281'
+          }
+        ]
+      }
+    ]
+  }
+}
---
+++ src/services/extenders/wikidataGeoProperties/requestTransformer.js
@@ -0,0 +1,27 @@
+import config from './index';
+import axios from 'axios';
+
+const {endpoint} = config.private;
+
+export default async (req) => {
+  const { items } = req.processed;
+
+  // for each column to extend
+  return Promise.all(Object.keys(items).map(async (colId) => {
+    const columnItems = items[colId];
+
+    const ids = Object.keys(columnItems).reduce((acc, metaId) => {
+      const [prefix, id] = metaId.split(':');
+      if (prefix === 'wd') {
+        acc.push(id);
+      }
+      return acc;
+    }, []);
+
+    const res = await axios.get(`${endpoint}${ids.join('|')}`);
+    
+    return {
+      res: res.data
+    }
+  }));
+}
