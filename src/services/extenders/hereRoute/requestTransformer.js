61a235e4aaa8ea8872264518d84a505874dfbf04
Stefano Marzorati <marzoste3@gmail.com>
2022-06-17 15:29:01 +0200

    add here services

---
+++ src/services/extenders/hereRoute/requestTransformer.js
@@ -0,0 +1,61 @@
+import config from './index';
+import axios from 'axios';
+
+const {endpoint} = config.private;
+
+function cleanCoordinates(coordinates){
+  if(coordinates !== undefined){
+    coordinates = coordinates.split(':')[1].split(',');
+    return [parseFloat(coordinates[0]), parseFloat(coordinates[1])];
+  }
+  return undefined;
+}
+
+function getLatLongStart(start_row){
+  if(start_row !== undefined){
+    return cleanCoordinates(start_row);
+  }
+  return undefined;
+}
+
+function getLatLongEnd(end_row){
+  if(end_row[1][0] !== undefined){
+    return cleanCoordinates(end_row[1][0].id);
+  }
+  return undefined;
+}
+
+function createRoute(start, end){
+  if(start !== undefined && end !== undefined){
+    return {"origin": start, 
+  "destination": end}
+  }
+  return undefined;
+}
+
+export default async (req) => {
+
+
+  let RouteList = []
+  let RowDict = {}
+  
+  const {items} = req. original;
+  const { props } = req.original;
+
+  const start = items[Object.keys(items)[0]];
+  const end = props.end;
+
+  Object.keys(start).forEach(row => {
+      let route = createRoute(getLatLongStart(start[row]), getLatLongEnd(end[row]));
+      if(route !== undefined){
+        RouteList.push(route);
+        RowDict[row] = route;
+      }
+  })  
+  const res = await axios.post(endpoint, {'json': RouteList});
+  
+  return {'data': res.data, 'dict':RowDict, 'start': Object.keys(items)[0], end:props.end[Object.keys(props.end)[0]][2]};
+}
+
+
+
---
+++ src/services/reconciliators/hereGeoCoords/responseTransformer.js
@@ -0,0 +1,65 @@
+import config from './index';
+
+const { uri } = config.public;
+
+function getInformation(item){
+  return {"label": item.address.label, "lat": item.position.lat, "lng": item.position.lng, "score": item.scoring.queryScore, "name_eng": item.address.label}
+}
+
+
+function getDict(res){
+  let dict = {};
+  res.forEach(item => {
+    if(item.items[0] !== undefined){
+      dict[item.address] = getInformation(item.items[0]);
+    }
+  });
+  return dict
+}
+
+function getMetaData(data){
+    return [{
+    'id': String("here:"+data.lat+","+data.lng),
+    'feature': [ { 'id': 'all_labels', 'value': 100 } ],
+    'name': data.name_eng,
+    'score': data.score,
+    'match': true,
+    'type': [{'id': "wd:Q29934236", 'name': "GlobeCoordinate"}]
+  }]
+}
+
+function getColumnMetadata(){
+  return [{
+    'id': "wd:Q29934236",
+    'name': "GlobeCoordinate",
+    'score': 0,
+    'match': true,
+  }]
+}
+
+
+
+export default async (req, res) => {
+
+  const { items } = req.original;
+  let header = {}
+  header.id = items[0].id;
+  header.metadata = getColumnMetadata();
+  delete(items[0]);
+  const dict = getDict(res);
+  let response = [];
+  response.push(header)
+
+  items.forEach(item => {
+    let row = {};
+    row.id = item.id;
+    if(dict[item.label]){
+      row.metadata = getMetaData(dict[item.label]);
+    }else{
+      row.metadata = [];
+    }
+    response.push(row);
+  });
+  console.log(response[0].metadata[0]);
+  return response;
+}
