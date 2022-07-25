3f864af8c8cd287d4b85443e22bc951fa5307f41
Stefano Marzorati <marzoste3@gmail.com>
2022-07-25 11:16:43 +0200

    Add atoka services

---
+++ src/services/extenders/atokaExtender/responseTransformer.js
@@ -0,0 +1,170 @@
+function getAddress(data) {
+  return data["item"]["base"]["registeredAddress"]["fullAddress"];
+}
+
+function getTaxID(data) {
+  return data["item"]["base"]["taxId"];
+}
+
+function getAssets(data) {
+  let assets = "";
+  if (data["item"]["economics"]["balanceSheets"] === undefined) {
+    return ""
+  } else {
+    data = data["item"]["economics"]["balanceSheets"];
+    console.log(data)
+    data.forEach(item => {
+      if (item["latest"] === true) {
+        assets = item["assets"];
+      }
+    });
+    return assets
+
+
+  }
+}
+
+export default async (req, res) => {
+
+  const { props } = req.processed;
+  const property = props["property"];
+
+  const column_to_extend = Object.keys(req.processed.items)[0];
+
+  let response = {
+    columns: {},
+    meta: {}
+  }
+
+  property.forEach(prop => {
+    let label_column = prop + "_" + column_to_extend;
+    let colEntity = [];
+    let colProperty = [];
+    let colType = [];
+
+    //ENTITY COL
+
+    if (String(prop) === "fullAddress") {
+      colEntity = [{
+        "name": "address",
+        "id": "wd:Q319608",
+        "score": 100,
+        "match": true
+      }];
+      colProperty = [{
+        id: 'wd:P144',
+        obj: column_to_extend,
+        match: true,
+        name: 'based on',
+        score: 100
+      },
+      {
+        id: 'wd:P6375',
+        obj: column_to_extend,
+        match: true,
+        name: 'street address',
+        score: 100
+      }];
+      colType = [
+        {
+          "id": "wd:Q319608",
+          "name": "address",
+          "match": true,
+          "score": 100
+        }];
+    } else {
+      if (String(prop) === "assets") {
+        colEntity = [{
+          "name": "asset",
+          "id": "wd:Q46737",
+          "score": 100,
+          "match": true
+        }];
+        colProperty = [{
+          id: 'wd:P144',
+          obj: column_to_extend,
+          match: true,
+          name: 'based on',
+          score: 100
+        },{
+          id: 'wd:P2403',
+          obj: column_to_extend,
+          match: true,
+          name: 'total assets',
+          score: 100
+        }];
+        colType = [
+          {
+            "id": "wd:Q4916",
+            "name": "euro",
+            "match": true,
+            "score": 100
+          }];
+      } else {
+        colEntity = [{
+          "name": "taxpayer identification number",
+          "id": "wd:Q47159572",
+          "score": 100,
+          "match": true
+        }];
+        colProperty = [{
+          id: 'wd:P144',
+          obj: column_to_extend,
+          match: true,
+          name: 'based on',
+          score: 100
+        }];
+        colType = [
+          {
+            "id": "wd:Q47159572",
+            "name": "taxpayer identification number",
+            "match": true,
+            "score": 100
+          }];
+      }
+    }
+
+
+    response.columns[label_column] = {
+      label: label_column,
+      kind: 'literal',
+      metadata: [],
+      cells: {}
+    };
+
+    
+
+    response.columns[label_column].metadata.push({
+      "id": label_column,
+      "name": label_column,
+      "entity": colEntity,
+      "type": colType,
+      "property": colProperty
+    });
+
+    res[0].forEach(row => {
+      let label_result = ""
+      if (String(prop) === "fullAddress") {
+        label_result = getAddress(row)
+      } else {
+        if (String(prop) === "assets") {
+          label_result = getAssets(row);
+        } else {
+          label_result = getTaxID(row);
+        }
+      }
+      response.columns[label_column].cells[row["row"]] = {
+        label: label_result,
+        metadata: []
+      }
+    });
+
+
+  })
+
+  console.log(column_to_extend)
+
+  return response;
+}
+
+
