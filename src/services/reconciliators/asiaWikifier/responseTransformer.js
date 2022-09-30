50bbda8be4913238be94be239ae368cd34733776
Stefano Marzorati <marzoste3@gmail.com>
2022-09-30 16:36:24 +0200

    add asia reconciliator

---
+++ src/services/reconciliators/asiaWikifier/responseTransformer.js
@@ -0,0 +1,22 @@
+import config from './index';
+
+const { uri } = config.public;
+
+export default async (req, res) => {
+  const { items } = req.processed;
+
+  const response = Object.keys(res).flatMap((label) => {
+    const metadata = res[label].result.map(({ id, ...rest }) => ({
+      id: `dbp:${id}`,
+      ...rest
+    }))
+
+    return items[label].map((cellId) => ({
+      id: cellId,
+      metadata
+    }))
+  });
+
+  return response;
+}
+
