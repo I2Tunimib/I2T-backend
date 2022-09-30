d381afdffcddb703947d9881a245c5e1f74bacd6
Stefano Marzorati <marzoste3@gmail.com>
2022-09-30 16:32:27 +0200

    add ASIA services

---
+++ src/services/extenders/asiaGeonames/index.js
@@ -0,0 +1,43 @@
+export default {
+  private: {
+    endpoint: process.env.ASIA_EXTENSION,
+    processRequest: true
+  },
+  public: {
+    name: 'ASIA (geonames)',
+    relativeUrl: '/asia/geonames',
+    description: 'ASIA extension service based on geonames allows to extend a column with data on locations of a certain administrative order. The input column has to be reconciliated against geonames entities.',
+    formParams: [
+      {
+        id: 'property',
+        description: 'Select on or more <b>Property</b> values:',
+        label: 'Property',
+        inputType: 'checkbox',
+        rules: ['required'],
+        options: [
+          {
+            id: 'adm1',
+            label: 'First-order administrative division (Regions or States)',
+            value: 'parentADM1'
+          },
+          {
+            id: 'adm2',
+            label: 'Second-order administrative division (Provinces)',
+            value: 'parentADM2'
+          },
+          {
+            id: 'adm3',
+            label: 'Third-order administrative division (Communes)',
+            value: 'parentADM3'
+          },
+          {
+            id: 'adm4',
+            label: 'Fourth-order administrative division',
+            value: 'parentADM4'
+          }
+        ]
+      }
+    ]
+  }
+
+}
