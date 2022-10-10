246075cddfd46c95d445f10b2d7b0a14a2c6b729
Stefano Marzorati <marzoste3@gmail.com>
2022-10-10 10:20:01 +0200

    Add restart all to docker compose

--- src/services/extenders/hereRoute/responseTransformer.js
+++ src/services/extenders/hereRoute/responseTransformer.js
@@ -45,104 +45,101 @@ export default async (req, res) => {
 
 
   property.forEach(prop => {
-    if (prop !== 'poi') {
+    response.columns[prop] = {
+      label: prop,
+      kind: 'literal',
+      entity: [],
+      metadata: [],
+      cells: {}
+    }
 
+    const colProperty = [{
+      id: 'P1427',
+      obj: start_label,
+      match: true,
+      name: 'start point',
+      score: 100
+    },
+    {
+      id: 'P1444',
+      obj: end_label,
+      match: true,
+      name: 'destination point',
+      score: 100
+    }];
 
-      response.columns[prop] = {
-        label: prop,
-        kind: 'literal',
-        entity: [],
-        metadata: [],
-        cells: {}
-      }
-
-      const colProperty = [{
-        id: 'P1427',
-        obj: start_label,
-        match: true,
-        name: 'start point',
-        score: 100
-      },
-      {
-        id: 'P1444',
-        obj: end_label,
-        match: true,
-        name: 'destination point',
-        score: 100
-      }];
-
-      let colType = "";
-      let colEntity = "";
-      if (prop === "duration") {
+    let colType = "";
+    let colEntity = "";
+    if (prop === "duration") {
+      colType = [
+        {
+          "id": "wd:Q7727",
+          "name": "minute",
+          "match": true,
+          "score": 100
+        }];
+      colEntity = [
+        {
+          "name": "duration",
+          "id": "wd:Q2199864",
+          "score": 100,
+          "match": true
+        }
+      ];
+    } else {
+      if (prop === "length") {
         colType = [
           {
-            "id": "wd:Q7727",
-            "name": "minute",
+            "id": "wd:Q828224",
+            "name": "kilometre",
             "match": true,
             "score": 100
           }];
         colEntity = [
           {
-            "name": "duration",
-            "id": "wd:Q2199864",
+            "name": "length",
+            "id": "wd:Q36253",
             "score": 100,
             "match": true
           }
         ];
       } else {
-        if (prop === "length") {
-          colType = [
-            {
-              "id": "wd:Q828224",
-              "name": "kilometre",
-              "match": true,
-              "score": 100
-            }];
-          colEntity = [
-            {
-              "name": "length",
-              "id": "wd:Q36253",
-              "score": 100,
-              "match": true
-            }
-          ];
-        } else {
-          colType = [
-            {
-              "id": "wd:Q111226201",
-              "name": "MultiLineString",
-              "match": true,
-              "score": 100
-            }];
-          colEntity = [
-            {
-              "name": "itinerary",
-              "id": "wd:Q1322323",
-              "score": 100,
-              "match": true
-            }];
-        }
+        colType = [
+          {
+            "id": "wd:Q111226201",
+            "name": "MultiLineString",
+            "match": true,
+            "score": 100
+          }];
+        colEntity = [
+          {
+            "name": "itinerary",
+            "id": "wd:Q1322323",
+            "score": 100,
+            "match": true
+          }];
       }
-
-
-      response.columns[prop].metadata[0] = {
-        "id": "path_" + start_label + "_" + end_label,
-        "name": prop,
-        "entity": colEntity,
-        "type": colType,
-        "property": colProperty
-      }
-
-
-      Object.keys(dict).forEach(index => {
-        let row_id = dict[index];
-        let label_result = getPropRoute(res[index], prop)
-        response.columns[prop].cells[row_id] = {
-          label: label_result,
-          metadata: []
-        }
-      });
     }
+
+
+    response.columns[prop].metadata[0] = {
+      "id": "path_" + start_label + "_" + end_label,
+      "name": prop,
+      "entity": colEntity,
+      "type": colType,
+      "property": colProperty
+    }
+
+
+    Object.keys(dict).forEach(index => {
+      let row_id = dict[index];
+      let label_result = getPropRoute(res[index], prop)
+      response.columns[prop].cells[row_id] = {
+        label: label_result,
+        metadata: []
+      }
+    });
+
   });
 
   return response;
