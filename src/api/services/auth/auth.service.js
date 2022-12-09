8dbc9ff3b1f82b5410a507f5ae158f399175e17c
Marco Ripamonti <m.ripamonti@campus.unimib.it>
2022-12-09 15:44:37 +0100

    Add auth

---
+++ src/api/services/auth/auth.service.js
@@ -0,0 +1,17 @@
+import config from "../../../config/index";
+import jwt from 'jsonwebtoken';
+
+const {
+  JWT_SECRET
+} = config
+
+
+const AuthService = {
+  verifyToken: (req) => {
+    const token = String(req?.headers?.authorization?.replace('Bearer ', ''));
+    const decoded = jwt.verify(token, JWT_SECRET);
+    return decoded;
+  }
+};
+
+export default AuthService;
