f893cb1d0abde83fcaa9af673a026fde9f9bb65b
Stefano Marzorati <marzoste3@gmail.com>
2023-06-20 14:11:41 +0200

    update inteface atoka people match

--- src/services/reconciliators/atokaPeople/index.js
+++ src/services/reconciliators/atokaPeople/index.js
@@ -31,57 +31,16 @@ const location = [{ 'id': '', 'label': ' ', 'value': '' },
 const company = [{ 'id': '', 'label': ' ', 'value': '' },
 { 'id': 'companies', 'label': 'Atoka : companies', 'value': 'companies' }]
 
-const servicesDescription = '<br>This external service is used to find the match between: <br> <b style="color: #3498db ; font-size: 25px;">Atoka</b> → people in the knowledge graph.<br><b style="color: #FF5733; font-size: 25px;">Source</b> → data contained in the table<br><br><hr style="width:108%; border:1px solid #e0e0e0;">';
-
-const firstDescription = '<h2>First</h2>The selected column in the Source table will be compared with the selected <b style="color: #3498db; font-size: 20px;">Atoka</b> filter:<br>';
-
-const relevantDescription = '<br>In this section is possible to set up the filters to use in the people search operation.<br><br><b>N.B</b> The selected column in the table contains data to use with first filter. <br><br><hr style="width:105%; border:1px solid #e0e0e0;">';
-
-const locationDescription = '<br><hr style="width:105%; border:1px solid #e0e0e0;"><br> <h1>Location Filters</h1> <br> in this section is possible to set up the location filters, location constraints to for a greater precision in the search operation.<br><br>';
-
-const companyDescription = '<br><hr style="width:105%; border:1px solid #e0e0e0;"><br> <h1>Company Filters</h1> <br> in this section is possible to set up the company filters, company constraints to for a greater precision in the search operation.<br><br>';
-
-
-function sourceDescription(filterName) {
-  return  'after the choice of '+ filterName + ' filter, you can select the <b style="color: #FF5733; font-size: 20px;">Source</b> column with the data:';
-}
-
-function atokaDescription(filterName) {
-  return '<hr style="width:105%; border:1px solid #e0e0e0;"> <h2>' + filterName + '</h2> Select the <b style="color: #3498db; font-size: 20px;">Atoka</b> filter to use in the match:';
-}
-
-function atokaSelect(id, description, options) {
-  return {
-    id: id,
-    description: description,
-    label: "Atoka",
-    infoText: "",
-    inputType: 'select',
-    rules: [],
-    options: options
-  }
-}
-
-function sourceSelect(id, description) {
-  return {
-    id: id,
-    description: description,
-    label: 'Source',
-    infoText: '',
-    inputType: 'selectColumns',
-    rules: []
-  }
-}
 
 export default {
   private: {
     endpoint: process.env.ATOKA_PEOPLE,
     access_token: process.env.ATOKA_TOKEN,
-    processRequest: true,
+    processRequest: false,
     min_threshold: 0.6
   },
   public: {
-    name: 'Atoka People',
+    name: 'Atoka People Match',
     description: '',
     relativeUrl: '/atokaPeople',
     prefix: 'atokaPeople',
@@ -106,43 +65,78 @@ export default {
         type: 'tag'
       }
     },
-    formParams: [
-      {
-        id: 'atokaFirstRel',
-        description: servicesDescription + relevantDescription + firstDescription,
-        label: "Atoka",
-        infoText: "",
-        inputType: 'select',
-        rules: ['required'],
-        options: generalInfo
+    formSchema:{
+      firstFilter: {
+        title: "<h2>Starting Filter</h2>",
+        description: "Select the <b style='color: #3498db ; font-size: 20px;'>type</b> of data in the table column.",
+        component: "group",
+        dynamic: false,
+        fields : {
+          column: {
+            label : "Table Column",
+            component: "select",
+            rules: ['required'],
+            options: [{ 'id': 'colonnaSpec', 'label': 'Selected Column', 'value': 'selected_column' }]
+          },
+          type: {
+            label: "Data Type",
+            component: "select",
+            rules: ['required'],
+            options: generalInfo
+          }
+        }
       },
-
-
-      atokaSelect('atokaSecondRel', atokaDescription('Second'), generalInfo),
-
-      sourceSelect('SecondRel', sourceDescription('Second')),
-
-      atokaSelect('atokaThirdRel', atokaDescription('Third'), generalInfo),
-
-      sourceSelect('ThirdRel', sourceDescription('Third')),
-
-      atokaSelect('atokaFourthRel', atokaDescription('Fourth'), generalInfo),
-
-      sourceSelect('FourthRel', sourceDescription('Fourth')),
-
-      atokaSelect('atokaFiveRel', locationDescription + atokaDescription('First'), location),
-
-      sourceSelect('FiveRel', sourceDescription('First')),
-
-      atokaSelect('atokaSixRel', atokaDescription('Second'), location),
-
-      sourceSelect('SixRel', sourceDescription('Second')),
-      
-      atokaSelect('atokaSevRel', companyDescription + atokaDescription('First'), company),
-
-      sourceSelect('SevRel', sourceDescription('First')),
-
-
-    ]
+      generalInfoFilter: {
+        title: "<h2>General Info Filter</h2>",
+        description: "<br>In this section is possible to set up the filters to use in the people search operation.<br><br><b>N.B</b> The selected column in the table contains data to use with first filter. <br><br><hr style='width:105%; border:1px solid #e0e0e0;'>",
+        component: "group",
+        dynamic: true,
+        fields : {
+          column: {
+            label : "Table Column",
+            component: "selectColumns"
+          },
+          type: {
+            label: "Data Type",
+            component: "select",
+            options: generalInfo
+          }
+        }
+      },
+      locationFilter: {
+        title: "<h2>Location Filter</h2>",
+        component: "group",
+        description: 'In this section is possible to set up the location filters, location constraints to for a greater precision in the search operation.',
+        dynamic: true,
+        fields : {
+          column: {
+            label : "Table Column",
+            component: "selectColumns"
+          },
+          type: {
+            label: "Data Type",
+            component: "select",
+            options: location
+          }
+        }
+      },
+      companyFilter: {
+        title: "<h2>Company Filter</h2>",
+        component: "group",
+        description: 'In this section is possible to set up the company filters, company constraints to for a greater precision in the search operation.',
+        dynamic: true,
+        fields : {
+          column: {
+            label : "Table Column",
+            component: "selectColumns"
+          },
+          type: {
+            label: "Data Type",
+            component: "select",
+            options: company
+          }
+        }
+      }
+    }
   }
 }
