c6e9927e9af462e38e13a895aca93fc805f7c8c1
Blowdire <agazzi.ruben99@gmail.com>
2025-04-07 14:16:27 +0200

    removed old form params

--- src/services/reconciliators/geonames/index.js
+++ src/services/reconciliators/geonames/index.js
@@ -1,64 +1,51 @@
 export default {
-    private: {
-        endpoint: process.env.GEONAMES,
-        access_token: process.env.GEONAMES_TOKEN,
-        processRequest: true
+  private: {
+    endpoint: process.env.GEONAMES,
+    access_token: process.env.GEONAMES_TOKEN,
+    processRequest: true,
+  },
+  public: {
+    name: "Linking: GeoNames (GeoNames)",
+    description:
+      "A geographic reconciliation and linking service that adds GeoNames IDs, labels, and " +
+      "descriptions. <br>" +
+      "<br><strong>Input</strong>: A column with mentions (strings) to reconcile, and possibly " +
+      "more columns to set a context for more accurate results. " +
+      "<br><strong>Output</strong>: Annotations associated with column cells in W3C compliant format.",
+    relativeUrl: "/dataset",
+    prefix: "geo",
+    uri: "http://www.geonames.org/",
+    metaToView: {
+      id: {
+        label: "ID",
+      },
+      name: {
+        label: "Name",
+        type: "link",
+      },
+      score: {
+        label: "Score",
+      },
+      type: {
+        label: "Types",
+        type: "subList",
+      },
+      match: {
+        label: "Match",
+        type: "tag",
+      },
     },
-    public: {
-        name: 'Linking: GeoNames (GeoNames)',
-        description: 'A geographic reconciliation and linking service that adds GeoNames IDs, labels, and ' +
-            'descriptions. <br>' +
-            '<br><strong>Input</strong>: A column with mentions (strings) to reconcile, and possibly ' +
-            'more columns to set a context for more accurate results. ' +
-            '<br><strong>Output</strong>: Annotations associated with column cells in W3C compliant format.',
-        relativeUrl: '/dataset',
-        prefix: 'geo',
-        uri: 'http://www.geonames.org/',
-        metaToView: {
-            id: {
-                label: 'ID',
-            },
-            name: {
-                label: 'Name',
-                type: 'link'
-            },
-            score: {
-                label: 'Score'
-            },
-            type: {
-                label: 'Types',
-                type: 'subList'
-            },
-            match: {
-                label: 'Match',
-                type: 'tag'
-            }
-        },
-        formParams: [
-            {
-                id: 'secondPart',
-                description: "Optional column to add information to support reconciliation.",
-                label: "Select a column with information about the location to reconcile",
-                infoText: "",
-                inputType: 'selectColumns',
-                rules: []
-            },
-            {
-                id: 'thirdPart',
-                description: "Optional column to add information to support reconciliation.",
-                label: "Select a column with information about the location to reconcile",
-                infoText: "",
-                inputType: 'selectColumns',
-                rules: []
-            },
-            {
-                id: 'fourthPart',
-                description: "Optional column to add information to support reconciliation.",
-                label: "Select a column with information about the location to reconcile",
-                infoText: "",
-                inputType: 'selectColumns',
-                rules: []
-            }
-        ]
-    }
-}
+    formParams: [
+      {
+        id: "additionalColumns",
+        description:
+          "Optional columns to add information to support reconciliation.",
+        label:
+          "Select columns with information about the location to reconcile",
+        infoText: "",
+        inputType: "multipleColumnSelect",
+        rules: [],
+      },
+    ],
+  },
+};
