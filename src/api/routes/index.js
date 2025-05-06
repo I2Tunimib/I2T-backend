0e51efe81e4f56e39ec9d35525f18657214f6c3e
Blowdire <agazzi.ruben99@gmail.com>
2025-05-06 18:52:19 +0200

    registration routes

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -5,6 +5,7 @@ import datasetsRoutes from "./datasets.route.js";
 import extensionRoutes from "./extension.route.js";
 import authRoutes from "./auth.route.js";
 import suggestionRoutes from "./suggestion.route.js";
+
 const router = Router();
 
 router.get("/", (req, res) => {
@@ -18,5 +19,4 @@ router.use("/extenders", extensionRoutes);
 router.use("/dataset", datasetsRoutes);
 router.use("/auth", authRoutes);
 router.use("/suggestion", suggestionRoutes);
-
 export default router;
