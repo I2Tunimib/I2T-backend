39502c18fb3872780a9e483ca833b19e9ab73627
Blowdire <agazzi.ruben99@gmail.com>
2025-05-28 11:36:01 +0200

    updated wikidata annotation properties

--- src/services/extenders/reconciledColumnExtWikidata/responseTransformer.js
+++ src/services/extenders/reconciledColumnExtWikidata/responseTransformer.js
@@ -12,15 +12,17 @@ export default async (req, res) => {
   // input columns ids from the request items
   const inputColumnName = Object.keys(items)[0];
 
-  console.log(`*** Label Extender *** inputColumn: ${JSON.stringify(inputColumnName)}`);
+  console.log(
+    `*** Label Extender *** inputColumn: ${JSON.stringify(inputColumnName)}`
+  );
 
   const response = {
     // columns entities to be added
     columns: {},
     // mapping between the new column obtained from extension of the input column (i.e.: { newColumnId: inputColumnId })
     // meta is used to place the new columns in the correct order in the UI.
-    meta: {}
-  }
+    meta: {},
+  };
 
   for (const label of props.labels) {
     const newColumnName = label + "_" + inputColumnName;
@@ -28,95 +30,104 @@ export default async (req, res) => {
     newColumn[newColumnName] = {
       label: newColumnName,
       metadata: [],
-      cells: {}
+      cells: {},
     };
     // console.log(`*** Label Extender *** rowCell: ${JSON.stringify(newColumn)}`);
     // Add rowCell to the columns object
     response.columns = { ...newColumn, ...response.columns };
     // Update meta to put the new column next to the inputColumnName
     // response.meta[newColumnName] = inputColumnName;
-    response.meta = {[newColumnName]: inputColumnName, ...response.meta };
+    response.meta = { [newColumnName]: inputColumnName, ...response.meta };
   }
 
-  for (const key in wikidata) { // for each entity
+  for (const key in res) {
+    // for each entity
     // console.log(`*** Label Extender *** key: ${JSON.stringify(key)}`);
     // console.log(`*** Label Extender *** items: ${JSON.stringify(items)}`);
-    const rowIdKey = items.buyer[`wd:${key}`]; // all rows with entity identified by key
+    const buyerItems = items[inputColumnName] || {};
+    const rowIdKey = buyerItems[`wd:${key}`] || buyerItems[`wdA:${key}`] || [];
+
+    // all rows with entity identified by key
     // console.log(`*** Label Extender *** entityIdKey: ${JSON.stringify(rowIdKey)}`);
-    for (const row of rowIdKey) { // for each row with entity identified by key
-      for (const property of props.labels) {  // for each requested property
+    for (const row of rowIdKey) {
+      // for each row with entity identified by key
+      for (const property of props.labels) {
+        // for each requested property
         const newColumnName = property + "_" + inputColumnName;
         const newCellContent = {
           label: "",
-          metadata: []
+          metadata: [],
         };
         switch (property) {
           case "id":
             newCellContent.label = key;
             break;
           case "url":
-            newCellContent.label = wikidata[key].url;
+            newCellContent.label = res[key].url;
             break;
           case "name":
-            newCellContent.label = wikidata[key].labels.en;
+            newCellContent.label = res[key].labels.en;
             break;
           case "description":
-            newCellContent.label = wikidata[key].description;
+            newCellContent.label = res[key].description;
         }
         // console.log(`*** Label Extender *** newCellContent: ${JSON.stringify(newCellContent)}`);
         const newCell = {
-          [row]: { ...newCellContent }
+          [row]: { ...newCellContent },
         };
         // console.log(`*** Label Extender *** newCell: ${JSON.stringify(newCell)}`);
-        response.columns[newColumnName].cells = { ...response.columns[newColumnName].cells, ...newCell };
+        response.columns[newColumnName].cells = {
+          ...response.columns[newColumnName].cells,
+          ...newCell,
+        };
       }
     }
   }
 
   const responseConst = {
-    "columns": {
-      "id_buyer": {
-        "label": "id_buyer",
-        "metadata": [],
-        "cells": {
-          "r2": {
-            "label": "Q4916650",
-            "metadata": []
-          }
-        }
+    columns: {
+      id_buyer: {
+        label: "id_buyer",
+        metadata: [],
+        cells: {
+          r2: {
+            label: "Q4916650",
+            metadata: [],
+          },
+        },
       },
-      "url_buyer": {
-        "label": "url_buyer",
-        "metadata": [],
-        "cells": {
-          "r2": {
-            "label": "https://www.wikidata.org/wiki/Q4916650",
-            "metadata": []
-          }
-        }
+      url_buyer: {
+        label: "url_buyer",
+        metadata: [],
+        cells: {
+          r2: {
+            label: "https://www.wikidata.org/wiki/Q4916650",
+            metadata: [],
+          },
+        },
       },
-      "name_buyer": {
-        "label": "name_buyer",
-        "metadata": [],
-        "cells": {
-          "r2": {
-            "label": "birmingham city council",
-            "metadata": []
-          }
-        }
+      name_buyer: {
+        label: "name_buyer",
+        metadata: [],
+        cells: {
+          r2: {
+            label: "birmingham city council",
+            metadata: [],
+          },
+        },
       },
-      "description_buyer": {
-        "label": "description_buyer",
-        "metadata": [],
-        "cells": {
-          "r2": {
-            "label": "local government body for the English city",
-            "metadata": []
-          }
-        }
-      }
+      description_buyer: {
+        label: "description_buyer",
+        metadata: [],
+        cells: {
+          r2: {
+            label: "local government body for the English city",
+            metadata: [],
+          },
+        },
+      },
     },
-    "meta": {}
+    meta: {},
   };
 
   // fs.writeFile('../../fileSemTUI/responseEXT-UI-labels.json',
@@ -126,4 +137,4 @@ export default async (req, res) => {
   //     });
 
   return response;
-}
+};
