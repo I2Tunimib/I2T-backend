054bc1397237850e7f5ffff8232fbed31764bd3d
Blowdire <agazzi.ruben99@gmail.com>
2025-09-23 10:05:59 +0200

    some fixes for configuration

--- config.js
+++ config.js
@@ -2,25 +2,39 @@ import atokaPeopleExtender from "./src/services/extenders/atokaPeopleExtender/in
 
 export default {
   // path to dataset files relative to root folder
-  datasetFilesPath: '/public/datasets',
+  datasetFilesPath: "/public/datasets",
   // path to dataset db relative to root folder
-  datasetDbPath: '/public/datasets.info.json',
+  datasetDbPath: "/public/datasets.info.json",
   // path to tables db relative to root folder
-  tablesDbPath: '/public/tables.info.json',
+  tablesDbPath: "/public/tables.info.json",
   // path to folder with temporary files
-  tmpPath: '/tmp',
+  tmpPath: "/tmp",
   // path to users db relative to root folder
-  usersPath: '/public/users.info.json',
+  usersPath: "/public/users.info.json",
 
   services: {
     // path to services relative to src folder
-    path: '/services',
+    path: "/services",
     // specify services to exclude during config initialization
     // excluded services won't be loaded during app startup
     exclude: {
-      extenders: [".DS_Store", "asiaGeonames", "asiaWeather", "atokaExtender", "atokaPeopleExtender",
-        "parametricWikiDataSelectSPARQL", "parametricWikiDataSPARQL", "wikidataGeoPropertiesSPARQL"],
-      reconciliators: [".DS_Store", "asiaKeywordsMatcher", "asiaWikifier", "atokaMatch2", "atokaPeople"]
-    }
-  }
-}
+      extenders: [
+        ".DS_Store",
+        "asiaGeonames",
+        "asiaWeather",
+        "atokaExtender",
+        "atokaPeopleExtender",
+        "parametricWikiDataSelectSPARQL",
+        "parametricWikiDataSPARQL",
+        "wikidataGeoPropertiesSPARQL",
+      ],
+      reconcilers: [
+        ".DS_Store",
+        "asiaKeywordsMatcher",
+        "asiaWikifier",
+        "atokaMatch2",
+        "atokaPeople",
+      ],
+    },
+  },
+};
