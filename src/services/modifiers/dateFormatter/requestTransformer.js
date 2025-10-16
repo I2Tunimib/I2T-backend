edc5ff8715d228a264dad8cf7d4b06bc60ac08bf
ccyrenee <96290089+ccyrenee@users.noreply.github.com>
2025-10-16 12:51:26 +0200

    feat: add radio to create new column or edit existing ones

--- src/services/modifiers/dateFormatter/requestTransformer.js
+++ src/services/modifiers/dateFormatter/requestTransformer.js
@@ -1,12 +1,13 @@
 export default async (req) => {
   const { props } = req.original;
-  const { formatType, customPattern, detailLevel } = props;
+  const { formatType, customPattern, detailLevel, outputMode } = props;
 
   return {
     props: {
       formatType: formatType || "iso",
       customPattern: customPattern || "",
       detailLevel: detailLevel || "date",
+      outputMode: outputMode,
     },
   };
 };
