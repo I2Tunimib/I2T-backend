6b3fd9fab6ddc52892c25784fdb842f2735ee470
ccyrenee <96290089+ccyrenee@users.noreply.github.com>
2025-11-22 12:52:08 +0100

    fix: fetch type description for MetadataDialog (cell, column)

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -6,6 +6,7 @@ import extensionRoutes from "./extension.route.js";
 import modificationRoutes from "./modification.route.js";
 import authRoutes from "./auth.route.js";
 import suggestionRoutes from "./suggestion.route.js";
+import metadataRoutes from "./metadata.route.js";
 
 const router = Router();
 
@@ -21,4 +22,5 @@ router.use("/modifiers", modificationRoutes);
 router.use("/dataset", datasetsRoutes);
 router.use("/auth", authRoutes);
 router.use("/suggestion", suggestionRoutes);
+router.use("/metadata", metadataRoutes);
 export default router;
