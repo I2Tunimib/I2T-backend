7a9e9bc4fad9988daf97364dc8fc1b1246e4832a
ccyrenee <96290089+ccyrenee@users.noreply.github.com>
2025-11-24 16:50:24 +0100

    fix: updated extenders' description

--- src/services/extenders/geoPropertiesWikidata/index.js
+++ src/services/extenders/geoPropertiesWikidata/index.js
@@ -7,12 +7,18 @@ export default {
     uri: "https://www.wikidata.org/wiki/",
     name: "Geo Properties (Wikidata)",
     relativeUrl: "/wikidata/entities",
-    description:
-      "Add properties associated with wikidata entities. Note that some required properties may be missing in Wikidata.",
+    description: "An extender that retrieves geographic properties from Wikidata for the entities in the selected " +
+      "reconciled column.<br><br>" +
+      "<strong>Input</strong>: A <em>column reconciled against Wikidata</em>, with entities' ID in any supported " +
+      "format (e.g., <code>wd:Q42</code>), plus a <em>selection of geographic properties</em>, such as coordinates, " +
+      "time zone, postal code.<br>" +
+      "<strong>Output</strong>: One new column for each selected property, populated with the corresponding values " +
+      "retrieved from Wikidata.<br><br>" +
+      "<strong>Note</strong>: Some entities may lack one or more of the requested properties in Wikidata.",
     formParams: [
       {
         id: "property",
-        description: "Select on or more <b>Property</b> values:",
+        description: "Select one or more <b>properties</b>:",
         label: "Property",
         inputType: "checkbox",
         rules: ["required"],
