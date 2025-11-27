1d1095d7a47920fac4b86ac5faeabc6a8006973b
Blowdire <agazzi.ruben99@gmail.com>
2025-11-27 17:33:03 +0100

    fixed reconciledColumnExt name and id to use already present metadata

--- src/services/extenders/reconciledColumnExt/index.js
+++ src/services/extenders/reconciledColumnExt/index.js
@@ -1,37 +1,39 @@
 export default {
   private: {
     endpoint: "",
-    processRequest: true
+    processRequest: true,
   },
   public: {
-    name: 'Annotation properties',
-    relativeUrl: '',
-    description: 'An extender that consolidates existing linking annotations by generating new column(s) containing ' +
-      '<em>ID</em> and/or <em>name</em> values from the reconciled column.<br><br>' +
-      '<strong>Input</strong>: A <em> reconciled column </em> against any dataset or knowledge graph; ' +
-      'a <em> selection of the properties </em> to extract (ID in any supported format <code>prefix:id</code>, ' +
-      'name as string).<br> <strong>Output</strong>: One new column for each requested property, containing the ' +
-      'extracted metadata from the reconciled entities.',
+    allValues: true,
+    name: "Annotation properties",
+    relativeUrl: "",
+    description:
+      "An extender that consolidates existing linking annotations by generating new column(s) containing " +
+      "<em>ID</em> and/or <em>name</em> values from the reconciled column.<br><br>" +
+      "<strong>Input</strong>: A <em> reconciled column </em> against any dataset or knowledge graph; " +
+      "a <em> selection of the properties </em> to extract (ID in any supported format <code>prefix:id</code>, " +
+      "name as string).<br> <strong>Output</strong>: One new column for each requested property, containing the " +
+      "extracted metadata from the reconciled entities.",
     formParams: [
       {
-        id: 'property',
-        description: 'Select one or more <b>properties</b>:',
-        label: 'Property',
-        inputType: 'checkbox',
-        rules: ['required'],
+        id: "property",
+        description: "Select one or more <b>properties</b>:",
+        label: "Property",
+        inputType: "checkbox",
+        rules: ["required"],
         options: [
           {
-            id: 'id',
-            label: 'ID of entities in the reference dataset',
-            value: 'id'
+            id: "id",
+            label: "ID of entities in the reference dataset",
+            value: "id",
           },
           {
-            id: 'name',
-            label: 'Name of entities in the reference dataset',
-            value: 'name'
-          }
-        ]
-      }
-    ]
-  }
-}
+            id: "name",
+            label: "Name of entities in the reference dataset",
+            value: "name",
+          },
+        ],
+      },
+    ],
+  },
+};
