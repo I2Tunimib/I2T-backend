6730aedb122b9ca113a1dbc08291c9243f712964
Blowdire <agazzi.ruben99@gmail.com>
2025-12-01 12:45:33 +0100

    restored support for previous handling of reconciled column ext alongside with new version for compatibiity

--- src/services/extenders/reconciledColumnExt/responseTransformer.js
+++ src/services/extenders/reconciledColumnExt/responseTransformer.js
@@ -1,38 +1,57 @@
 import fetch from "node-fetch";
+import fs from "fs";
 
-// async function getName(id) {
-//   let result = "";
-//   if (id) {
-//     if (id.startsWith("wd:")) {
-//       const cleanId = id
-//         .replace(/^wd:/, "")
-//         .replace(/^wdA:/, "")
-//         .replace(/^wdL:/, "")
-//         .trim();
-//       const url = `https://www.wikidata.org/wiki/Special:EntityData/${cleanId}.json`;
-//       const res = await fetch(url);
-//       const data = await res.json();
-//       const entity = data.entities?.[cleanId];
-//       result = entity?.labels?.en?.value || "";
-//     } else if (id.startsWith("geo:")) {
-//       const endpoint = process.env.GEONAMES;
-//       const token = process.env.GEONAMES_TOKEN;
-//       const cleanId = id.replace(/^geo:/, "").trim();
-//       const url = `${endpoint}/getJSON?geonameId=${cleanId}&username=${token}`;
-//       const res = await fetch(url);
-//       const item = await res.json();
-//       result = item.name;
-//     }
-//   }
-//   return String(result);
-// }
-function getName(metaObj) {
+// OLD IMPLEMENTATION FUNCTIONS (when selectedColumns is passed)
+function getNameOld(row) {
+  let result = "";
+  if (row[1] !== []) {
+    row[1].forEach((element) => {
+      if (element.match === true) {
+        result = element.name.value;
+      }
+    });
+  } else {
+    return "";
+  }
+  return String(result);
+}
+
+function getIdOld(row) {
+  let result = "";
+  if (row[1] !== []) {
+    row[1].forEach((element) => {
+      if (element.match === true) {
+        result = element.id.split(":")[1];
+      }
+    });
+  } else {
+    return "";
+  }
+  return String(result);
+}
+
+function getRowDictOld(column) {
+  let dict = {};
+  Object.keys(column).forEach((row) => {
+    if (column[row] !== undefined) {
+      dict[row] = {
+        name: getNameOld(column[row]),
+        id: getIdOld(column[row]),
+      };
+    }
+  });
+  return dict;
+}
+
+// NEW IMPLEMENTATION FUNCTIONS (when selectedColumns is NOT passed)
+function getNameNew(metaObj) {
   if (metaObj.name) {
     if (metaObj.name.value) return metaObj.name.value;
     else return "N/A";
   } else return "N/A";
 }
-function getId(metaObj) {
+
+function getIdNew(metaObj) {
   if (metaObj.kbId) {
     if (metaObj.kbId.startsWith("http")) return metaObj.kbId;
     else
@@ -43,57 +62,86 @@ function getId(metaObj) {
   return;
 }
 
-async function getRowDict(column) {
+async function getRowDictNew(column) {
   const dict = {};
   for (const row of Object.keys(column)) {
     if (column[row] !== undefined) {
       dict[row] = {
-        name: await getName(column[row]),
-        id: getId(column[row]),
+        name: await getNameNew(column[row]),
+        id: getIdNew(column[row]),
       };
     }
   }
   return dict;
 }
 
+// COMMON FUNCTION
 function getLabel(dict, prop, row) {
   return dict[row][prop];
 }
 
-export default async function responseTransformer(req, res) {
+export default async (req, res) => {
   const { items, props } = req.original;
   const { selectedColumns } = props;
-
-  if (!selectedColumns || selectedColumns.length === 0) {
-    throw new Error(
-      "At least one column must be selected before running the operation.",
-    );
-  }
   const property = res.property;
 
-  const response = { columns: {}, meta: {} };
+  let response = {
+    columns: {},
+    meta: {},
+  };
 
-  for (const col of selectedColumns) {
-    for (const prop of property) {
-      const label_column = `${prop}_${col}`;
-      response.columns[label_column] = {
-        label: label_column,
-        metadata: [],
-        cells: {},
-      };
-
-      const columnData = items[col];
-      const dictRow = await getRowDict(columnData);
-
-      for (const row_id of Object.keys(columnData)) {
-        const label_result = getLabel(dictRow, prop, row_id);
-        response.columns[label_column].cells[row_id] = {
-          label: label_result,
+  // OLD IMPLEMENTATION (when selectedColumns is passed)
+  if (selectedColumns && selectedColumns.length > 0) {
+    selectedColumns.forEach((col) => {
+      property.forEach((prop) => {
+        let label_column = prop + "_" + col;
+        console.log("label_column", label_column);
+        response.columns[label_column] = {
+          label: label_column,
           metadata: [],
+          cells: {},
         };
+
+        const columnData = items[col];
+        const dictRow = getRowDictOld(columnData);
+
+        Object.entries(columnData).forEach(([row_id]) => {
+          let label_result = getLabel(dictRow, prop, row_id);
+          console.log("label_result", label_result);
+          response.columns[label_column].cells[row_id] = {
+            label: label_result,
+            metadata: [],
+          };
+        });
+      });
+    });
+  }
+  // NEW IMPLEMENTATION (when selectedColumns is NOT passed)
+  else {
+    const columns = Object.keys(items);
+
+    for (const col of columns) {
+      for (const prop of property) {
+        const label_column = `${prop}_${col}`;
+        response.columns[label_column] = {
+          label: label_column,
+          metadata: [],
+          cells: {},
+        };
+
+        const columnData = items[col];
+        const dictRow = await getRowDictNew(columnData);
+
+        for (const row_id of Object.keys(columnData)) {
+          const label_result = getLabel(dictRow, prop, row_id);
+          response.columns[label_column].cells[row_id] = {
+            label: label_result,
+            metadata: [],
+          };
+        }
       }
     }
   }
 
   return response;
-}
+};
