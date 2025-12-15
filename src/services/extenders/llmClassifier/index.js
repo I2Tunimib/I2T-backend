cc68e858f3414bbfd611dc9164cf4db1ca3caeb0
ccyrenee <96290089+ccyrenee@users.noreply.github.com>
2025-12-15 22:12:35 +0100

    moved link in COFOG description

--- src/services/extenders/llmClassifier/index.js
+++ src/services/extenders/llmClassifier/index.js
@@ -7,14 +7,14 @@ export default {
     name: "COFOG Classifier",
     relativeUrl: "",
     description: "A classification service that assigns a government department or public organization to the most " +
-      "appropriate category among the ten top-level COFOG (Classification of the Functions of Government) options. " +
+      "appropriate category among the ten top-level COFOG (Classification of the Functions of Government - For more " +
+      "details, click <a href='https://en.wikipedia.org/wiki/Classification_of_the_Functions_of_Government' " +
+      "target='_blank'>here</a>.) options. " +
       "The classification is based on the organization's name, description, country, and Wikidata metadata.<br><br>" +
       "<strong>Input</strong>: Organization details including <em>name</em>, <em>description</em>, <em>country</em> " +
       "and optional <em>Wikidata fields</em> such as description or type.<br>" +
       "<strong>Output</strong>: A JSON object containing the predicted COFOG category (<code>cofog_label: 01-10</code>), " +
-      "confidence level (high/medium/low) and reasoning notes.<br><br>" +
-      "For more details, click <a href='https://en.wikipedia.org/wiki/Classification_of_the_Functions_of_Government' " +
-      "target='_blank'>here</a>.",
+      "confidence level (high/medium/low) and reasoning notes.<br><br>",
     formParams: [
       {
         id: "description",
