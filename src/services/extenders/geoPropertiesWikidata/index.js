f7913efeab8f207caf600548eae18e58b36a17b4
ccyrenee <96290089+ccyrenee@users.noreply.github.com>
2025-12-17 13:13:47 +0100

    fix: updated extenders' description specifying properties/params selected

--- src/services/extenders/geoPropertiesWikidata/index.js
+++ src/services/extenders/geoPropertiesWikidata/index.js
@@ -10,8 +10,12 @@ export default {
     description: "An extender that retrieves geographic properties from Wikidata for the entities in the selected " +
       "reconciled column.<br><br>" +
       "<strong>Input</strong>: A <em>column reconciled against Wikidata</em>, with entities' ID in any supported " +
-      "format (e.g., <code>wd:Q42</code>), plus a <em>selection of geographic properties</em>, such as coordinates, " +
-      "time zone, postal code.<br>" +
+      "format (e.g., <code>wd:Q42</code>), plus a <em>selection of geographic properties</em>:" +
+      "<ul style='list-style-type: disc;'>" +
+        "<li>Coordinate location (latitude & longitude)</li>" +
+        "<li>Time zone</li>" +
+        "<li>Postal code</li>" +
+      "</ul>" +
       "<strong>Output</strong>: One new column for each selected property, populated with the corresponding values " +
       "retrieved from Wikidata.<br><br>" +
       "<strong>Note</strong>: Some entities may lack one or more of the requested properties in Wikidata.",
--- src/services/extenders/reconciledColumnExt/index.js
+++ src/services/extenders/reconciledColumnExt/index.js
@@ -9,8 +9,12 @@ export default {
     description: 'An extender that consolidates existing linking annotations by generating new column(s) containing ' +
       '<em>ID</em> and/or <em>name</em> values from the reconciled column.<br><br>' +
       '<strong>Input</strong>: A <em> reconciled column </em> against any dataset or knowledge graph; ' +
-      'a <em> selection of the properties </em> to extract (ID in any supported format <code>prefix:id</code>, ' +
-      'name as string).<br> <strong>Output</strong>: One new column for each requested property, containing the ' +
+      'a <em> selection of the properties</em>:' +
+      '<ul style="list-style-type: disc;">' +
+        '<li>ID, in any supported format <code>prefix:id</code></li>' +
+        '<li>Name, as string</li>' +
+      '</ul>' +
+      '<strong>Output</strong>: One new column for each requested property, containing the ' +
       'extracted metadata from the reconciled entities.',
     formParams: [
       {
